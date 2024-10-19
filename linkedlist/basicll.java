dbdccc25fbd39f2ae9ee50b928678290b72e47cf
Anurag Singh <anuragsingh815311@gmail.com>
2024-10-19 22:23:21 +0530

    linked list basics

--- linkedlist/basicll.java
+++ linkedlist/basicll.java
@@ -45,11 +45,11 @@ public class basicll {
         return count;
     }
 
-    void insert(Node head, int pos, int data) {
+    void insert(Node head, int pos, int data) { // will not work for pos==0 as we have not intialized
         Node newNode = new Node(data);
         Node temp = head;
         if (pos == 0) {
-            newNode.next = temp;
+            newNode.next = head;
             head = newNode;
         } else {
             for (int i = 0; i < pos - 1; i++) {
@@ -85,6 +85,7 @@ public class basicll {
         // System.out.println();
         // DisplayR(a);
         ob.insert(a, 3, 34);
+        ob.insert(a, 1, 120);
         ob.insert_at_end(a, 21);
         Display(a);
     }
