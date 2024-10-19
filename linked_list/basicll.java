cf87533a3b7c051f748076c7823fdc40022d6a6e
Anurag Singh <anuragsingh815311@gmail.com>
2024-10-19 18:23:31 +0530

    Linked List

---
+++ linked_list/basicll.java
@@ -0,0 +1,17 @@
+package linked_list;
+
+public class basicll {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
