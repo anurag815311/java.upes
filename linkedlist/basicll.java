4bbfd88860df17879ab84ac9527d169eaeb881e5
Anurag Singh <anuragsingh815311@gmail.com>
2024-10-19 20:59:55 +0530

    linked list basics

---
+++ linkedlist/basicll.java
@@ -0,0 +1,63 @@
+package linkedlist;
+
+public class basicll {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void Display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void DisplayR(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.print(head.data + " ");
+        DisplayR(head.next);
+
+    }
+
+    public static void DisplayRev(Node head) {
+        if (head == null) {
+            return;
+        }
+        DisplayRev(head);
+        System.out.print(head.data + " ");
+
+    }
+
+    public static int len(Node head) {
+        int count = 0;
+        while (head != null) {
+            count++;
+            head = head.next;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        Display(a);
+        System.out.println();
+        DisplayR(a);
+    }
+
+}
